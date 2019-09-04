41b10f303c1d5551cabf75a0159dc8febe939ef5
yangxiaolang <1810849666@qq.com>
2019-09-04 16:50:09 +0800

    上传页面添加没卵用的动画效果

--- pages/addItem/addItem.js
+++ pages/addItem/addItem.js
@@ -1,66 +1,25 @@
-// pages/addItem/addItem.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    animation:""
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShow:function(){
+    var left = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'liner',
+      delay: 0
+    });
+    left.rotate(180*2).step();
+    this.setData({
+      left:left.export()
+    })
+    var right = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'liner',
+      delay: 0
+    });
+    right.rotate(-180*2).step();
+    this.setData({
+      right:right.export()
+    })
   }
 })
